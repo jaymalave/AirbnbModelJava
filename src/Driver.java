9f2a6a316a3973b098f1cbdd86fbb8aa34316548
jaymalave <jaym.github@gmail.com>
2021-11-18 15:23:48 +0530

    registration input done

--- src/Driver.java
+++ src/Driver.java
@@ -1,9 +1,24 @@
+import java.util.Scanner;
 
 public class Driver {
   public static void main(String args[]){
 	  Utils utils = new Utils();
-	  utils.registerUser();
-	  utils.registerHost();
-	  utils.registerHouse();
+	  Scanner sc = new Scanner(System.in);
+	  System.out.println("Press 1 to register the user, 2 to register house and, 3 for the host");
+	  int reg_opt = sc.nextInt();
+	  switch(reg_opt) {
+	  case 1:
+		  utils.registerUser();
+		  break;
+	  case 2:
+		  utils.registerHouse();
+		  break;
+	  case 3:
+		  utils.registerHost();
+		  break;
+	  default:
+		  System.out.println("Enter a valid choice");
+		  break;
+	  }
   }
 }
